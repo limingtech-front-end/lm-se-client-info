6ee86a689b16790abac36472719e5208e36ec722
limingtech <limingtech@hotmail.com>
2017-01-03 18:54:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import checkClient from 'ut-check-client'
+import calcUriParams from 'ut-check-uri-params'
+
+let isIos=checkClient.isIos,
+	isAndroid=checkClient.isAndroid,
+	isWechatWebView=checkClient.isWechat
+
+let isAndroidWebViewUriPrefix=calcUriParams.getUriQuery().isAndroid,
+	isIosWebViewUriPrefix=calcUriParams.getUriQuery().isIos
+	
+const renders={
+	iosWebView:'iosWebView',
+	androidWebView:'androidWebView',
+	wechatWebView:'wechatWebView',
+	browser:'browser'
+}
+
+function isIosWebView(){
+	// return isIos()	
+	return !!isIosWebViewUriPrefix
+}
+
+function isAndroidWebView(){
+	// return isAndroid() 
+	return !!isAndroidWebViewUriPrefix
+}
+
+function maybeBrowser(){
+	return !isWechatWebView() && !isIosWebView() && !isAndroidWebView()
+}
+
+const render={
+	isAndroidWebView:isAndroidWebView(),
+	isIosWebView:isIosWebView(),
+	isNativeWebView:isAndroidWebView() || isIosWebView(),
+	isWechatWebView:isWechatWebView(),
+	isAndroid:isAndroid(),
+	isIos:isIos(),
+	isBrowser:maybeBrowser()
+}
+
+console.log(render)
+export default{
+	renders:renders,
+	render:render
+}
